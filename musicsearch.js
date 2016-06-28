b5e2425657ce2f651e18b63adf72df79df2d3d12
Dan Ballard <ich.atomkind@gmail.com>
2016-06-27 18:02:36 -0600

    musicsearch push 2

--- musicsearch.js
+++ musicsearch.js
@@ -10,12 +10,15 @@ function drawSongs(songList){
   
   for (var i = 0; i < songList.length; i++) {
     var currentSong = songList[i]; 
-    songListElem.innerHTML += "<h3>"+currentSong.title+"</h3>>";
-    songListElem.innerHTML += "<h4>"+currentSong.artist+"</h4>";
-    songListElem.innerHTML += "<img src="+currentSong.albumArt+" alt= album art>" + "</img>";
-    songListElem.innerHTML += "<h5>"+currentSong.collection+"</h5>";
-    songListElem.innerHTML += "<h5>"+currentSong.price+"</h5>";
-    songListElem.innerHTML += '<audio controls = "controls" preload="none"><source src="' +currentSong.preview+ '" type=audio/mp4></audio>';
+    var song = '<div class="col-sm-4">'
+    song += "<h3>"+currentSong.title+"</h3>>";
+    song += "<h4>"+currentSong.artist+"</h4>";
+    song += "<img src="+currentSong.albumArt+" alt= album art>" + "</img>";
+    song += "<h5>"+currentSong.collection+"</h5>";
+    song += "<h5>"+currentSong.price+"</h5>";
+    song += '<audio controls = "controls" preload="none"><source src="' +currentSong.preview+ '" type=audio/mp4></audio></div>';
+
+    songListElem.innerHTML += song
   } 
 
   
